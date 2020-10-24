ef2304155e9e8b3935522b9f2f8e8c85a6ff9b61
romenhurst <44817261+romenhurst@users.noreply.github.com>
2020-10-24 00:18:05 -0400

    Make Dirt 1.0.0

---
+++ src/MakeDirt/Patch.cs
@@ -0,0 +1,54 @@
+using Harmony;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+
+namespace MakeDirt
+{
+	[HarmonyPatch(typeof(RockCrusherConfig))]
+	[HarmonyPatch("ConfigureBuildingTemplate")]
+    public static class RockCrusherConfig_ConfigureBuildingTemplate_Patch
+    {
+		public static LocString DIRT_RECIPE_DESCRIPTION = "Crushes {0}, {1}, and {2} into {3}";
+
+		public static void Postfix(GameObject __0)
+		{
+			var clay = ElementLoader.elements.Find((Element e) => e.HasTag(SimHashes.Clay.CreateTag()));
+			var sand = ElementLoader.elements.Find((Element e) => e.HasTag(SimHashes.Sand.CreateTag()));
+			var phosphorite = ElementLoader.elements.Find((Element e) => e.HasTag(GameTags.Phosphorite));
+			var dirt = ElementLoader.elements.Find((Element e) => e.HasTag(GameTags.Dirt));
+
+			if (clay != null && sand != null && phosphorite != null)
+			{
+				ComplexRecipe.RecipeElement[] inputs = new ComplexRecipe.RecipeElement[3]
+				{
+					new ComplexRecipe.RecipeElement(sand.tag, 80f),
+					new ComplexRecipe.RecipeElement(clay.tag, 10f),
+					new ComplexRecipe.RecipeElement(phosphorite.tag, 10f)
+				};
+
+				ComplexRecipe.RecipeElement[] outputs = new ComplexRecipe.RecipeElement[1]
+				{
+					new ComplexRecipe.RecipeElement(dirt.tag, 100f)
+				};
+
+				string text = ComplexRecipeManager.MakeRecipeID("RockCrusher", inputs, outputs);
+				new ComplexRecipe(text, inputs, outputs)
+				{
+					time = 40f,
+					description = string.Format(DIRT_RECIPE_DESCRIPTION, clay.name, sand.name, phosphorite.name, dirt.name),
+	
+					nameDisplay = ComplexRecipe.RecipeNameDisplay.IngredientToResult,
+					fabricators = new List<Tag>
+					{
+						TagManager.Create("RockCrusher")
+					}
+				};
+			}
+		}
+    }
+}
