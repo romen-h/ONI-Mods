82f6e9df7243483d76c55a8e84d398311b0a3e4a
romenhurst <44817261+romenhurst@users.noreply.github.com>
2021-11-12 09:20:17 -0500

    Custom Audio

---
+++ src/CommonLib/CustomLoopingSounds.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FMOD;
+
+using FMODUnity;
+
+using UnityEngine;
+
+namespace RomenH.CommonLib
+{
+	public class CustomLoopingSounds : KMonoBehaviour, IRenderEveryTick
+	{
+		public string soundName;
+
+		public bool shouldPauseOnGamePause = true;
+
+		public bool enableCulling = true;
+
+		public bool enableCameraScaling = true;
+
+		public float volume = 1f;
+
+		public float falloffDistanceSq = 64f;
+
+		public Vector3 velocity = Vector3.zero;
+
+		private FMOD.Channel sound;
+
+		protected override void OnSpawn()
+		{
+			base.OnSpawn();
+
+			sound = AudioUtil.CreateSound("PistonLoop");
+		}
+
+		public void RenderEveryTick(float dt)
+		{
+			SoundCuller soundCuller = CameraController.Instance.soundCuller;
+
+			if (shouldPauseOnGamePause && Time.timeScale == 0f)
+			{
+				sound.setPaused(true);
+				return;
+			}
+
+			bool audible = !enableCulling || (enableCameraScaling && soundCuller.IsAudible(transform.position, falloffDistanceSq)) || soundCuller.IsAudibleNoCameraScaling(transform.position, falloffDistanceSq);
+			if (!audible)
+			{
+				sound.setPaused(true);
+				return;
+			}
+
+			sound.setPaused(false);
+
+			Vector3 soundPos = transform.position;
+			soundPos.z = 0;
+
+			float velocityScale = TuningData<LoopingSoundManager.Tuning>.Get().velocityScale;
+			VECTOR scaledSoundPos = SoundEvent.GetCameraScaledPosition(soundPos, false).ToFMODVector();
+			VECTOR scaledSoundVel = (velocity * velocityScale).ToFMODVector();
+			sound.set3DAttributes(ref scaledSoundPos, ref scaledSoundVel);
+		}
+
+		protected override void OnCleanUp()
+		{
+			base.OnCleanUp();
+
+			if (sound.hasHandle())
+			{
+				sound.isPlaying(out bool playing);
+				if (playing) sound.stop();
+				sound.clearHandle();
+			}
+		}
+	}
+}
