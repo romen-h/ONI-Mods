5a317e98615f0327be7ca720a0bb904ced68a9e8
romen-h <44817261+romen-h@users.noreply.github.com>
2026-07-16 22:16:35 -0400

    Added SetupSymlinks.ps1 helper script

---
+++ src/SetupSymlinks.ps1
@@ -0,0 +1,33 @@
+$CD = $PSScriptRoot
+Set-Location -Path $CD
+
+$DocumentsFolder = [Environment]::GetFolderPath('MyDocuments')
+$ONIFolder = "${DocumentsFolder}/Klei/OxygenNotIncluded"
+$ModsFolder = "${ONIFolder}/mods"
+$DevFolder = "${ModsFolder}/Dev"
+
+if (!Test-Path -Path $DevFolder) {
+	New-Item -ItemType "Directory" -Path $DevFolder
+}
+
+$Projects = @(
+	'DumpingSign',
+	'FanTiles',
+	'GermicideLamp',
+	'LogicScheduleSensor',
+	'MakeDirt',
+	'PipedDeodorizer',
+	'PlasticDoor',
+	'PlasticTiles',
+	'PlasticUtilities',
+	'SoggyCarpets',
+	'StirlingEngine',
+	'TECBlock',
+	'Thresholds'
+)
+
+foreach ($Project in $Projects) {
+	$SrcFolder = "${CD}/${Project}/bin"
+	$DestFolder = "${DevFolder}/${Project}"
+	New-Item -ItemType Junction -Path $DestFolder -Target $SrcFolder
+}
